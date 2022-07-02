bbd1bd3a6681205e72435f34af069418be647e34
Sandra Schuhmacher <coding@voiding-warranties.de>
2022-07-02 21:54:47 +0200

    adjusted solution

--- src/iSpindel.Core/BrewBatchAggregate/RezeptSchritt.cs
+++ src/iSpindel.Core/BrewBatchAggregate/RezeptSchritt.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using iSpindel.SharedKernel;
 
 namespace iSpindel.Core.BrewBatchAggregate;
 public class RezeptSchritt : ValueObject
