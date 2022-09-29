fc1823147f3e6ca88dc8c86de8d5df9be46ebec6
Sandra Schuhmacher <Sandra.Schuhmacher@sva.de>
2022-09-29 13:10:35 +0200

    fixed crosscompile build for data-ingestor and mqtt-producer

--- build-containers.sh
+++ build-containers.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-
-CURRENT_ARCH=$(uname -m)
-BASE_IMAGE_ARCH=
-DOTNET_ARCH=
 REGISTRY=localhost:5000
+#REGISTRY=hauspi:6000
 
-if [[ "$CURRENT_ARCH" == *"x86"* ]]; then
-    BASE_IMAGE_ARCH=""
-    DOTNET_ARCH="x64"
+echo ">> Building iSpindel.Server, the Data Ingestor"
+echo "> Step 1/2 - x86_64 Build"
+docker buildx build -q --build-arg DOTNET_ARCH=x64 --build-arg BASE_IMAGE_ARCH="" -f Docker/Dockerfile_data-ingestor -t $REGISTRY/ispindel-server:v1.0 --push .
+
+echo "> Step 2/2 - arm Build"
+docker buildx build -q --platform=linux/arm/v7 --build-arg DOTNET_ARCH="arm" --build-arg BASE_IMAGE_ARCH="-arm32v7" -f Docker/Dockerfile_data-ingestor -t $REGISTRY/ispindel-server:v1.0 --push .
+
+echo ">> Building iSpindel.App, the Webservice"
+echo "> Step 1/2 - x86_64 Build"
+docker buildx build -q --build-arg DOTNET_ARCH=x64 --build-arg BASE_IMAGE_ARCH="" -f Docker/Dockerfile_webapp -t $REGISTRY/ispindel-webapp:v1.0 --push .
+
+echo "> Step 2/2 - arm Build"
+docker buildx build -q --platform=linux/arm/v7 --build-arg DOTNET_ARCH="arm" --build-arg BASE_IMAGE_ARCH="-arm32v7" -f Docker/Dockerfile_webapp -t $REGISTRY/ispindel-webapp:v1.0 --push .
+
+if [[ -d "../MQTT-Generator" ]]; then
+    echo ">> Building the MQTT Payload Generator"
+    echo "> Step 1/2 - x86_64 Build"
+    docker buildx build -q --build-arg DOTNET_ARCH="x64" -f ../MQTT-Generator/Dockerfile -t $REGISTRY/mqtt-producer:x64-v1.0 --push ../MQTT-Generator
+
+    echo "> Step 2/2 - arm Build"
+    docker buildx build -q --platform=linux/arm/v7 --build-arg DOTNET_ARCH="arm" --build-arg BASE_IMAGE_ARCH="-arm32v7" -f ../MQTT-Generator/Dockerfile -t $REGISTRY/mqtt-producer:arm-v1.0 --push ../MQTT-Generator
 fi
-
-if [[ "$CURRENT_ARCH" == *"arm"* ]]; then
-    BASE_IMAGE_ARCH="-arm32v7"
-    DOTNET_ARCH="arm"
-fi
-
-echo "Building for $CURRENT_ARCH, Base-Image: $BASE_IMAGE_ARCH, Dotnet: $DOTNET_ARCH"
-
-echo "Building iSpindel.Server, the Data Ingestor"
-docker build --build-arg DOTNET_ARCH=$DOTNET_ARCH --build-arg BASE_IMAGE_ARCH=$BASE_IMAGE_ARCH -f Docker/Dockerfile_data-ingestor . -t $REGISTRY/ispindel-server:v1.0
-docker push $REGISTRY/ispindel-server:v1.0
-
-echo "Building iSpindel.App, the Webservice"
-docker build --build-arg DOTNET_ARCH=$DOTNET_ARCH --build-arg BASE_IMAGE_ARCH=$BASE_IMAGE_ARCH -f Docker/Dockerfile_webapp . -t $REGISTRY/ispindel-webapp:v1.0
-docker push $REGISTRY/ispindel-webapp:v1.0
