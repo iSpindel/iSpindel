8ac402426fd38cfc81b0553e685a0ca40c2c3c9f
Sandra Schuhmacher <Sandra.Schuhmacher@sva.de>
2022-10-03 18:29:56 +0200

    updated payload generator

--- Docker/generate-payload.sh
+++ Docker/generate-payload.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 PUB_INTERVAL=3000
+PORT=5000
+HOST=localhost
 BAT_TOPIC=ispindel/iSpindel0/battery
 BAT_MEAN=50
 GRAV_TOPIC=ispindel/iSpindel0/gravity
@@ -8,30 +10,30 @@ TEMP_TOPIC=ispindel/iSpindel0/temperature
 TEMP_MEAN=20
 
 
-if [ $( docker inspect mqtt-producer --format {{.State.Status}} ) != "running" ]; then
+if [[ $( docker inspect mqtt-producer --format {{.State.Status}} ) != "running" ]]; then
   echo "Container mqtt-producer does not exist or is not running."
   echo "Are you missing a docker start mqtt-producer ?"
   exit;
 fi
 
 echo "Removing existing workers"
-curl --request PATCH localhost:8080/workers/stop
-curl --request PATCH localhost:8080/workers/delete
+curl --request PATCH $HOST:$PORT/workers/stop
+curl --request PATCH $HOST:$PORT/workers/delete
 
 echo "Creating Payload for Battery:"
 echo "Topic: $BAT_TOPIC, Mean: $BAT_MEAN"
-curl -X POST -H "Content-Type: application/json" -d '{ "SubscriptionTopic": "stest", "PayloadOptions": { "PayloadType" : "NormalDistribution", "PublishTopic" : "'$BAT_TOPIC'", "Mean" : '$BAT_MEAN', "Stddev" : 1 }, "ShapeOptions": { "ShapeType" : "Interval", "LengthInMs" : '$PUB_INTERVAL' }}' localhost:8080/workers/new
+curl -X POST -H "Content-Type: application/json" -d '{ "SubscriptionTopic": "stest", "PayloadOptions": { "PayloadType" : "NormalDistribution", "PublishTopic" : "'$BAT_TOPIC'", "Mean" : '$BAT_MEAN', "Stddev" : 1 }, "ShapeOptions": { "ShapeType" : "Interval", "LengthInMs" : '$PUB_INTERVAL' }}' $HOST:$PORT/workers/new
 echo ""
 
 echo "Creating Payload for Gravity:"
 echo "Topic: $GRAV_TOPIC, Mean: $GRAV_MEAN"
-curl -X POST -H "Content-Type: application/json" -d '{ "SubscriptionTopic": "stest", "PayloadOptions": { "PayloadType" : "NormalDistribution", "PublishTopic" : "'$GRAV_TOPIC'", "Mean" : '$GRAV_MEAN', "Stddev" : 1 }, "ShapeOptions": { "ShapeType" : "Interval", "LengthInMs" : '$PUB_INTERVAL' }}' localhost:8080/workers/new
+curl -X POST -H "Content-Type: application/json" -d '{ "SubscriptionTopic": "stest", "PayloadOptions": { "PayloadType" : "NormalDistribution", "PublishTopic" : "'$GRAV_TOPIC'", "Mean" : '$GRAV_MEAN', "Stddev" : 1 }, "ShapeOptions": { "ShapeType" : "Interval", "LengthInMs" : '$PUB_INTERVAL' }}' $HOST:$PORT/workers/new
 echo ""
 
 echo "Creating Payload for Temperature:"
 echo "Topic: $TEMP_TOPIC, Mean: $TEMP_MEAN"
-curl -X POST -H "Content-Type: application/json" -d '{ "SubscriptionTopic": "stest", "PayloadOptions": { "PayloadType" : "NormalDistribution", "PublishTopic" : "'$TEMP_TOPIC'", "Mean" : '$TEMP_MEAN', "Stddev" : 1 }, "ShapeOptions": { "ShapeType" : "Interval", "LengthInMs" : '$PUB_INTERVAL' }}' localhost:8080/workers/new
+curl -X POST -H "Content-Type: application/json" -d '{ "SubscriptionTopic": "stest", "PayloadOptions": { "PayloadType" : "NormalDistribution", "PublishTopic" : "'$TEMP_TOPIC'", "Mean" : '$TEMP_MEAN', "Stddev" : 1 }, "ShapeOptions": { "ShapeType" : "Interval", "LengthInMs" : '$PUB_INTERVAL' }}' $HOST:$PORT/workers/new
 echo ""
 
 echo "Starting all workers"
-curl --request PATCH localhost:8080/workers/start
+curl --request PATCH $HOST:$PORT/workers/start
